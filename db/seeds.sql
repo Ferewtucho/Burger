40884cc18793d5d349e75f5595e10cc0ebe6a770
Ferew Tucho <ferewtucho@Ferews-MacBook-Pro.local>
2018-11-07 11:02:45 -0500

    uploading burger to get before heroku

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Bacon Burgers');
+INSERT INTO burgers (name) VALUES ('Umami Burgers');
+INSERT INTO burgers (name, Devour) VALUES ('Pug Burger', true);
+INSERT INTO burgers (name, Devour) VALUES ('Green-Chile Burgers', true);
+INSERT INTO burgers (name, Devour) VALUES ('Minetta Burger', true);
+INSERT INTO burgers (name) VALUES ('All-American Hamburgers');
