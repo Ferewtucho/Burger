56790ece6d9eb757103cf7fa0de72fa298ddf584
Ferew Tucho <ferewtucho@Ferews-MacBook-Pro.local>
2018-11-07 12:34:46 -0500

    updated db syntax error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,7 @@
-INSERT INTO burgers (name) VALUES ('Bacon Burgers');
-INSERT INTO burgers (name) VALUES ('Umami Burgers');
-INSERT INTO burgers (name, Devour) VALUES ('Pug Burger', true);
-INSERT INTO burgers (name, Devour) VALUES ('Green-Chile Burgers', true);
-INSERT INTO burgers (name, Devour) VALUES ('Minetta Burger', true);
-INSERT INTO burgers (name) VALUES ('All-American Hamburgers');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Umami Burgers');
+INSERT INTO burgers (burger_name) VALUES ('Steak House Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Pug Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Green-Chile Burgers', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Minetta Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('All-American Hamburgers');
